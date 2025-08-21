fcee014c31002aeb3c93cc5901a07b9222be5dd5
joaol324j <joaol324j@gmail.com>
2025-08-21 11:17:10 -0300

    fix: api.tsx

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://10.101.105.129:8000/api',
+  baseURL: 'http://0.0.0.0:8000/api',
   timeout: 10000,
 });
 
